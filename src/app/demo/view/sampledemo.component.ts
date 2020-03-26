f800121d450b9804dcfbbaab64d94f610be49d04
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2020-03-26 13:41:55 +0000

    doc updated

--- src/app/demo/view/sampledemo.component.ts
+++ src/app/demo/view/sampledemo.component.ts
@@ -65,10 +65,6 @@ export class SampleDemoComponent implements OnInit {
 
     orderListCars: Car[];
 
-    carouselCars: Car[];
-
-    maskValue: string;
-
     toggleButtonChecked: boolean;
 
     selectedType: string;
@@ -213,18 +209,6 @@ export class SampleDemoComponent implements OnInit {
                     }
                 ]
             }
-        ];
-
-        this.carouselCars = [
-            {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
-            {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
-            {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
-            {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
-            {vin: 'gf45wg5', year: 2011, brand: 'Volkswagen', color: 'Red'},
-            {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
-            {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
-            {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
-            {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
         ];
     }
 
